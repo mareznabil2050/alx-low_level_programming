1f811ce0b633c03a7921d2842acc18e7ec8e7dd9
mareznabil2050 <mareznabil2050@gmail.com>
2023-04-24 16:32:53 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
