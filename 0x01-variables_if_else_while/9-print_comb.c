dedabe747a065a395c30a62d77b9b37ff6fa63b4
mareznabil2050 <mareznabil2050@gmail.com>
2023-04-24 16:34:49 -0700

    fix 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@
  */
 int main(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		putchar(c);
+		putchar('0' + c);
 		if (c != '9')
 		{
 			putchar(',');
